28e14a6e44d482c06130ac8a053340afc267d151
puneethprabhuk <puneethprabhuk@gmail.com>
2022-11-08 17:04:22 +0530

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <h1>My portfolio</h1>
+    <div className="main-container">
+      <Header />
+    </div>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return(
+        <div className='header-container'>
+            <div className="item-left">
+                <div className="header-item">&lt;//01.  Home&gt;</div>
+                <div className="header-item">&lt;//02.  About Me&gt;</div>
+            </div>
+            <div className="item-middle">
+                Puneeth Prabhu
+            </div>
+            <div className="item-right">
+                <div className="header-item">&lt;//03.  Projects&gt;</div>
+                <div className="header-item">&lt;//04.  Contact Me&gt;</div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
